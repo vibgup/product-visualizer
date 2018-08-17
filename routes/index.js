9a2ddb68d375d52db1ca48c8aa9f9b1c01d82f1f
vibhav <vibhav@gozefo.com>
2018-08-17 23:37:34 +0530

    added base routes

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,22 @@
-var express = require('express');
-var router = express.Router();
+const express = require('express');
+
+const router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
+router.get('/', (req, res) => {
   res.render('index', { title: 'Express' });
+});
+
+router.get('/visualizer/set_type', (req, res) => {
+  res.render('index', { title: 'Express' });
+});
+
+router.get('/visualizer/set_product', (req, res) => {
+  res.render('visualizer/product/index', { title: 'Express' });
+});
+
+router.get('/visualizer/viewer', (req, res) => {
+  res.render('visualizer/viewer/index', { title: 'Express' });
 });
 
 module.exports = router;
