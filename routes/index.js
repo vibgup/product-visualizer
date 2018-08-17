934842c233ae081e05e727818c8c3fd078c6d0dd
Swagnik Dutta <swagnikd@gmail.com>
2018-08-18 02:55:06 +0530

    removed route, middle page, added config

--- routes/index.js
+++ routes/index.js
@@ -1,22 +1,24 @@
 const express = require('express');
 
 const router = express.Router();
+const _ = require('lodash');
+
+const ImagesConfig = require('../config/index');
 
 /* GET home page. */
 router.get('/', (req, res) => {
   res.render('index', { title: 'Express' });
 });
 
-router.get('/visualizer/set_type', (req, res) => {
-  res.render('index', { title: 'Express' });
-});
-
-router.get('/visualizer/set_product', (req, res) => {
-  res.render('visualizer/product/index', { title: 'Express' });
-});
+// router.get('/visualizer/set_type', (req, res) => {
+//   res.render('index', { title: 'Express' });
+// });
 
 router.get('/visualizer/viewer', (req, res) => {
-  res.render('visualizer/viewer/index', { title: 'Express' });
+  res.render('visualizer/viewer/index', {
+    allSofas: _.get(ImagesConfig, 'sofas', []),
+    allTables: _.get(ImagesConfig, 'tables', []),
+  });
 });
 
 module.exports = router;
